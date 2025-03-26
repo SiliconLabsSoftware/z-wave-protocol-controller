a0dcacb2075d2968923cbff2b062a5b4013d1557
Thomas du Boisrouvray <thomas.duboisrouvray@silabs.com>
2025-03-26 11:07:39 +0100

    SWPROT-9418: Restore zwave_controller and create zwave_controller_testlib
    Create zwave_controller_testlib specifically to compile testlib.
    Keep all functionalities in zwave_controller lib

--- applications/zpc/components/zwave/zwave_controller/src/zwave_controller.c
+++ applications/zpc/components/zwave/zwave_controller/src/zwave_controller.c
@@ -2,7 +2,7 @@
  * # License
  * <b>Copyright 2021 Silicon Laboratories Inc. www.silabs.com</b>
  ******************************************************************************
- * The licensor of this software is Silicon Laboratories Inc. Your use of this
+* The licensor of this software is Silicon Laboratories Inc. Your use of this
  * software is governed by the terms of Silicon Labs Master Software License
  * Agreement (MSLA) available at
  * www.silabs.com/about-us/legal/master-software-license-agreement. This
@@ -23,8 +23,9 @@
 #include "ZW_classcmd.h"
 #include "zwave_s2_transport.h"
 
-// TODO: remove this include when compiling testlib
+#ifndef ZWAVE_TESTLIB
 #include "zpc_config.h"
+#endif
 
 // Includes from this component
 #include "zwave_controller_utils.h"
@@ -35,21 +36,30 @@
 // Setup Log ID
 #define LOG_TAG "zwapi_controller"
 
+#ifdef ZWAVE_TESTLIB
 static const zwave_controller_config_t* zwave_controller_config = NULL;
 
 void zwave_controller_set_config(const zwave_controller_config_t *config)
 {
   zwave_controller_config = config;
 }
+#endif // ZWAVE_TESTLIB
 
 void zwave_controller_set_application_nif(const uint8_t *command_classes,
                                           uint8_t command_classes_length)
 {
-  //TODO: solve this to build testlib
+#ifndef ZWAVE_TESTLIB
   node_type_t my_node_type
     = {.basic    = zpc_get_config()->zpc_basic_device_type,
        .generic  = zpc_get_config()->zpc_generic_device_type,
        .specific = zpc_get_config()->zpc_specific_device_type};
+#else // ZWAVE_TESTLIB
+  assert(zwave_controller_config != NULL);
+  node_type_t my_node_type
+    = {.basic    = zwave_controller_config->zpc_basic_device_type,
+       .generic  = zwave_controller_config->zpc_generic_device_type,
+       .specific = zwave_controller_config->zpc_specific_device_type};
+#endif // ZWAVE_TESTLIB
   // Tell the Z-Wave module what our NIF should be:
   zwapi_set_application_node_information(APPLICATION_NODEINFO_LISTENING,
                                          my_node_type,
